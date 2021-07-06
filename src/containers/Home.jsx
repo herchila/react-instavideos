e6d8928a0e1d71d7f18f87ba315a0757f37df26f
Hernan Chilabert <hchilabert@gmail.com>
2021-07-06 19:38:24 -0300

    Added layout component and login & register containers

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,49 +1,44 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return (
-    <div className="App">
-      <Header />
+    <>
       <Search />
-      {initialState.mylist.length > 0 &&
+      {initialState.mylist.length > 0 && (
         <Categories title="Mi Lista">
           <Carousel>
             <CarouselItem />
           </Carousel>
         </Categories>
-      }
+      )}
 
       <Categories title="Tendendias">
         <Carousel>
-          {initialState.trends.map(item =>
+          {initialState.trends.map((item) => (
             <CarouselItem key={item.id} {...item} />
-          )}
+          ))}
         </Carousel>
       </Categories>
 
       <Categories title="Originales">
         <Carousel>
-          {initialState.originals.map(item =>
+          {initialState.originals.map((item) => (
             <CarouselItem key={item.id} {...item} />
-          )}
+          ))}
         </Carousel>
       </Categories>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
-export default App;
+export default Home;
