407ec7ae70b78fbde60510586e8a449191f1eb13
Hernán Chilabert <hchilabert@gmail.com>
2021-07-08 00:45:30 -0300

    React Router & Redux (#1)
    * Added layout, login, register and favorite/delete events w/ actions & reducers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
   <header className="header">
-    <h1 className="header__h1">InstaVideos</h1>
+    <Link to="/">
+      <h1 className="header__h1">InstaVideos</h1>
+    </Link>
     <div className="header__menu">
       <div className="header__menu--profile">
         <img src={userIcon} alt="" />
         <p>Perfil</p>
       </div>
       <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
+        <li><Link to="/">Cuenta</Link></li>
+        <li><Link to="/login">Iniciar Sesión</Link></li>
       </ul>
     </div>
   </header>
---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <h1>404 Not Found</h1>
+    <p>Back to home</p>
+  </>
+);
+
+export default NotFound;
